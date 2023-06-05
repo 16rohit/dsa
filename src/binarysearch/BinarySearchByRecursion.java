e6758875063e928e514ff010f1fbc52fe40e4c80
rohitgupta <16rohitvicky@gmail.com>
2023-06-06 01:25:07 +0530

    binary search

--- src/binarysearch/BinarySearchByRecursion.java
+++ src/binarysearch/BinarySearchByRecursion.java
@@ -2,7 +2,7 @@ package binarysearch;
 
 public class BinarySearchByRecursion {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
         int n = 8;
         int ans = binarySearch(arr, 0, arr.length - 1, n);
         System.out.println(ans);
@@ -10,7 +10,7 @@ public class BinarySearchByRecursion {
 
     private static int binarySearch(int[] arr, int start, int end, int n) {
         if (start <= end) {
-            int mid = (start+end) / 2;
+            int mid = (start + end) / 2;
             if (arr[mid] == n) return mid;
             else if (arr[mid] > n) return binarySearch(arr, start, mid - 1, n);
             else return binarySearch(arr, mid + 1, end, n);
