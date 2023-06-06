f69cb2d2b1652eb2c1b9d8d0f5be3547d0fc3441
rohitgupta <16rohitvicky@gmail.com>
2023-06-07 00:05:33 +0530

    binary search and get count of the element

--- src/binarysearch/BinarySearchFirstOccurrence.java
+++ src/binarysearch/BinarySearchFirstOccurrence.java
@@ -4,7 +4,7 @@ public class BinarySearchFirstOccurrence {
     public static int ans = -1;
 
     public static void main(String[] args) {
-        int[] arr = {1, 2, 2, 2, 3, 3, 3,3, 3, 4, 5, 6, 7,7};
+        int[] arr = {1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 5, 6, 7, 7};
         int n = 7;
         binarySearch(arr, 0, arr.length - 1, n);
         System.out.println(ans);
