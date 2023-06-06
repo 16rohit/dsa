cade5a7ae786f9c7477ba30ab302668e41129eae
rohitgupta <16rohitvicky@gmail.com>
2023-06-07 01:16:13 +0530

    binary search and get no of times its repeated

---
+++ src/binarysearch/BinarySearchRotationCount.java
@@ -0,0 +1,24 @@
+package binarysearch;
+
+public class BinarySearchRotationCount {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int ans = binarySearch(arr, 0, arr.length - 1);
+        System.out.println(ans);
+    }
+
+    private static int binarySearch(int[] arr, int start, int end) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (mid == 0 || mid == arr.length - 1)
+                return mid;
+            else if (arr[mid] < arr[mid - 1] && arr[mid] < arr[mid + 1])
+                return mid;
+            else if (arr[mid] < arr[end])
+                return binarySearch(arr, start, mid - 1);
+            else
+                return binarySearch(arr, mid + 1, end);
+        }
+        return -1;
+    }
+}
