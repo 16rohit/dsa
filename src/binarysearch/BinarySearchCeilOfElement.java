8def74f3701e0f9483db27e4600975feac5f5738
rohitgupta <16rohitvicky@gmail.com>
2023-06-07 23:29:42 +0530

    binary search to find the ceil of the element

---
+++ src/binarysearch/BinarySearchCeilOfElement.java
@@ -0,0 +1,27 @@
+package binarysearch;
+
+public class BinarySearchCeilOfElement {
+    public static int ans = -1;
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6, 7};
+        binarySearch(arr, 0, arr.length - 1, 3);
+        System.out.println(ans);
+    }
+
+    private static void binarySearch(int[] arr, int start, int end, int n) {
+        if (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == n) {
+                ans = mid;
+            } else if (arr[mid] > n) {
+                ans = mid;
+                binarySearch(arr, start, mid - 1, n);
+            } else {
+                ans = mid +1;
+                binarySearch(arr, mid + 1, end, n);
+            }
+        }
+        if (ans >= arr.length) ans =-1;
+    }
+}
