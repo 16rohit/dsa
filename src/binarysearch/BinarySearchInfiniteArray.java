0c3bc85bfa76b5fba063c407d11a8414261e020c
rohitgupta <16rohitvicky@gmail.com>
2023-06-08 00:04:13 +0530

    binary search in infinite array

---
+++ src/binarysearch/BinarySearchInfiniteArray.java
@@ -0,0 +1,26 @@
+package binarysearch;
+
+public class BinarySearchInfiniteArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 3, 5, 6, 7};
+        for (int i = 0; i < arr.length; ++i) {
+            int ans = binarySearch(arr, 0, 2, arr[i]);
+            System.out.println(ans);
+        }
+    }
+
+    private static int binarySearch(int[] arr, int start, int end, int n) {
+        if (arr[end] == n)
+            return end;
+        else if (arr[start] <= n && arr[end] > n) {
+            int mid = (start + end) / 2;
+            if (arr[mid] == n)
+                return mid;
+            else if (arr[mid] > n)
+                return binarySearch(arr, start, mid - 1, n);
+            else
+                return binarySearch(arr, mid + 1, end, n);
+        } else
+            return binarySearch(arr, end + 1, end * 2, n);
+    }
+}
